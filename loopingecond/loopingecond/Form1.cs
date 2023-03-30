f3f541e0b0370f7481d2a57e31ea8641ce6707eb
João Henrique Leite <127433183+joaoleite2@users.noreply.github.com>
2023-03-30 12:02:22 -0300

    correções pastas

---
+++ loopingecond/loopingecond/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loopingecond
+{
+    public partial class frmmenu : Form
+    {
+        public frmmenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void combustívelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmcomb combustivel = new frmcomb();
+            combustivel.Show();
+            this.Hide();
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tabuadaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmloop looping = new frmloop();
+            looping.Show();
+            this.Hide();
+        }
+    }
+}
